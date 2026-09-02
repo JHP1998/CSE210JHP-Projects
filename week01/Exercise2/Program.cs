834fcbbaf1861d60c5bf5bf1f79d90ed660ece60
JHP1998 <jeprueitt98@gmail.com>
2026-09-01 21:03:50 -0600

    Removed Hello world statement and replaced with if, else if , and else if statements to determine grade based on user input

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage: ");
+        string response = Console.ReadLine();
+        int percentage = int.Parse(response);
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        
+         }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+     
 }
