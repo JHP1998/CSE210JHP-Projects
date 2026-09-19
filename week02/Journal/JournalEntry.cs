f4214e36e931960d35edcc48e5a788c91512d8f0
JHP1998 <jeprueitt98@gmail.com>
2026-09-19 10:50:09 -0600

    Created Journal entry class that accounts for journal entry dates, prompt and response to the journal entry prompts

---
+++ week02/Journal/JournalEntry.cs
@@ -0,0 +1,13 @@
+class JournalEntry
+{
+    public string Date { get; }
+    public string Prompt { get; }
+    public string Response { get; }
+
+    public JournalEntry(string date, string prompt, string response)
+    {
+        Date = date; 
+        Prompt = prompt;
+        Response = response;
+    }
+}
